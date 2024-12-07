b9af98d710dd6405ad027668d8a4763f361b40f8
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-12-07 15:41:40 +0100

    fix: footer alignment

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,7 +12,9 @@ export function Footer() {
 		>
 			<footer className="mt-24 pb-8 border-t">
 				<div className="max-w-screen-md mx-auto px-4 pt-8 flex items-center justify-between">
+					<span>
 					<SocialLinks />
+					</span>
 					<span> &copy; 2024 Ewere Diagboya</span>
 				</div>
 			</footer>
